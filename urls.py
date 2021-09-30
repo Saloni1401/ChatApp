b7fa96027db17413a86f9ed3360225805332788b
Saloni1401 <89349977+Saloni1401@users.noreply.github.com>
2021-09-30 23:43:22 +0530

    2

--- urls.py
+++ urls.py
@@ -1,14 +1,22 @@
-from django.contrib.auth.views import LogoutView
-from django.urls import path
-from . import views
+"""ChatApp URL Configuration
 
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('chat/', views.chat_view, name='chats'),
-    path('chat/<int:sender>/<int:receiver>/', views.message_view, name='chat'),
-    path('api/messages/<int:sender>/<int:receiver>/', views.message_list, name='message-detail'),
-    path('api/messages/', views.message_list, name='message-list'),
-    path('logout/', LogoutView.as_view(), name='logout'),
-    path('register/', views.register_view, name='register'),
+    path('admin/', admin.site.urls),
+    path('', include('chat.urls')),
 ]
